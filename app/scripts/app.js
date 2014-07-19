0fa5aec1cd1d27c758479b6ad60f13758e1ff7e5
vamshi <pvamshi@gmail.com>
2014-07-19 09:39:36 -0700

    search functionality with custom filter

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,15 +22,29 @@ angular
             restrict: 'AE',
             // replace: 'true',
             templateUrl: 'views/mainpage.html',
-            transclude : true,
-            controller: function($scope, $log) {
-                $scope.cat = function(val, event) {
-                    $log.info(val);
+            transclude: true,
+            controller: function($scope) {
+                $scope.cat = function(val) {
                     $scope.catval = val;
                 };
                 $scope.detail = function(val) {
                     $scope.detailval = val;
                 };
             }
+        };
+    })
+    .filter('itemSearch', function() {
+        return function(input, query) {
+            if (!query) {
+                return input;
+            }
+            var result = [];
+            angular.forEach(input, function(item) {
+                if (item.name && item.name.toLowerCase().indexOf(query) !== -1) {
+                    result.push(item);
+                }
+            });
+            return result;
+
         };
     });
